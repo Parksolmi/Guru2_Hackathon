1f98e7021f815ec5c8bd95b4f28fe70b636a751f
Parksolmi <7548ck@naver.com>
2022-07-31 18:46:25 +0900

    홈화면 버튼 이미지 추가

--- Moyeola/Moyeola/ViewController.swift
+++ Moyeola/Moyeola/ViewController.swift
@@ -18,6 +18,17 @@ class ViewController: UIViewController {
         view.addSubview(mapView)
         
     }
-
+    
+    /*
+    //네비게이션 바
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.navigationController?.setNavigationBarHidden(true, animated: false)
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.navigationController?.setNavigationBarHidden(false, animated: false)
+    }*/
+    
 }
 
