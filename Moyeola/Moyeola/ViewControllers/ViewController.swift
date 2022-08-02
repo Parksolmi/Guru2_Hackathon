5fe5ddc5cd38304b4faca7f085f3f877bcba53ff
Parksolmi <7548ck@naver.com>
2022-08-02 21:10:12 +0900

    중간 지점 찾기 어플 모여라

--- Moyeola/Moyeola/ViewControllers/ViewController.swift
+++ Moyeola/Moyeola/ViewControllers/ViewController.swift
@@ -7,30 +7,82 @@
 
 import UIKit
 import NMapsMap
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
     
     var datasource:[String] = []
+    
+    var mapView = NMFMapView(frame: UIScreen.main.bounds)
+    
+    //@IBOutlet weak var naverMapView: NMFNaverMapView!
+    
+    var locationManager = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        let mapView = NMFMapView(frame: view.frame)
+        let naverMapView = NMFNaverMapView(frame: view.frame)
+        
+        //사용자 UI
+        naverMapView.showCompass = true
+        naverMapView.showZoomControls = true
+        naverMapView.showLocationButton = true
+        
+        mapView = naverMapView.mapView
+        
+        //지도를 서브뷰로 넣기
         view.addSubview(mapView)
+        
+        
+        //현재 좌표 가져오기
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        //사용자 위치 허용 받는 팝업
+        locationManager.requestWhenInUseAuthorization()
+        
+        if CLLocationManager.locationServicesEnabled() {
+            NSLog("위치 서비스 On")
+            //위치 정보 받아오기 시작
+            locationManager.startUpdatingLocation()
+            print(locationManager.location?.coordinate)
+            
+            //현 위치로 카메라 이동
+            let cameraUpdate = NMFCameraUpdate(scrollTo: NMGLatLng(lat: locationManager.location?.coordinate.latitude ?? 0, lng: locationManager.location?.coordinate.longitude ?? 0))
+            
+            cameraUpdate.animation = .easeIn
+            mapView.moveCamera(cameraUpdate)
+            
+            let marker = NMFMarker()
+            marker.position = NMGLatLng(lat: locationManager.location?.coordinate.latitude ?? 0, lng: locationManager.location?.coordinate.longitude ?? 0)
+            marker.position = NMGLatLng(lat: 37.5670135, lng: 126.9783740)
+            marker.mapView = mapView
+            
+        } else {
+            NSLog("위치 서비스 Off")
+        }
         
     }
     
-    /*
-    //네비게이션 바
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        self.navigationController?.setNavigationBarHidden(true, animated: false)
+    
+    
+    //위치 정보 계속 업데이트
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        NSLog("didUpdateLocations")
+        if let location = locations.first {
+            NSLog("위도: \(location.coordinate.latitude)")
+            NSLog("경도: \(location.coordinate.latitude)")
+        }
     }
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        self.navigationController?.setNavigationBarHidden(false, animated: false)
-    }*/
+    
+    //위도 경도 받아오기 에러
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+    }
     
 }
+
+
+
 
